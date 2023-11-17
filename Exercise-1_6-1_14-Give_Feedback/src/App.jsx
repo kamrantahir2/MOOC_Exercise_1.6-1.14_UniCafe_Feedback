e98ea4c8115cbd7a013e6feaa2d84f28391e30ea
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-11-17 02:11:43 +0000

    Issue: good state not updating immediately. Counter on DOM does not match counter in console.log

--- Exercise-1_6-1_14-Give_Feedback/src/App.jsx
+++ Exercise-1_6-1_14-Give_Feedback/src/App.jsx
@@ -1,11 +1,86 @@
 import { useState } from "react";
 import "./App.css";
 
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+    </div>
+  );
+};
+
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [count, setCount] = useState(0);
+  const [average, setAverage] = useState(0);
+
+  const addGood = () => {
+    const added = good + 1;
+    setGood((prev) => prev + 1);
+    console.log("AddGood good:", good);
+    const countAdded = count + 1;
+    setCount(countAdded);
+    console.log("AddGood count:", count);
+    // calculateAverage();
+  };
+
+  const addNeutral = () => {
+    setNeutral(neutral + 1);
+    setCount(count + 1);
+  };
+
+  const addBad = () => {
+    setBad(bad + 1);
+    setCount(count + 1);
+  };
+
+  // const calculateAverage = () => {
+  //   const goodValue = good * 1;
+  //   const neutralValue = neutral * 0;
+  //   const badValue = bad * -1;
+
+  //   const total = goodValue + neutralValue + badValue;
+
+  //   console.log("goodValue", goodValue);
+
+  //   console.log("Total", total);
+
+  //   console.log("Count", count);
+
+  //   const calculated = total / count;
+
+  //   setAverage(calculated);
+
+  //   console.log("Average", average);
+  // };
+
   return (
     <>
       <div>
-        <h1>TEST</h1>
+        <Header />
+
+        <div>
+          <Button handleClick={addGood} text="Good" />
+          <Button handleClick={addNeutral} text="Neutral" />
+          <Button handleClick={addBad} text="Bad" />
+        </div>
+
+        <div>
+          <h1>Statistics</h1>
+          <div>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+            <p>All: {count}</p>
+            <p>Average: {average}</p>
+          </div>
+        </div>
       </div>
     </>
   );
